721805f7680908981767863f67069e32cbcf658e
educep <edoo@melix.org>
2025-03-14 10:57:26 +0100

    removing unuseful code

--- ai_prompts/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Created by Analitika at 19/11/2024
-contact@analitika.fr
-"""
-
-from ai_prompts.prompt_manager import PromptManager
-
-__all__ = ["PromptManager"]
--- aisuite/framework/__init__.py
+++
@@ -1,4 +0,0 @@
-from .chat_completion_response import ChatCompletionResponse
-from .provider_interface import ProviderInterface
-
-__all__ = ["ChatCompletionResponse", "ProviderInterface"]
